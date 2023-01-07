504a8b107a8c80dc8fc7d822317f68bcd42945cb
teresa113697 <teresa2002@hotmail.nl>
2023-01-07 03:31:30 +0100

    Added Timer
    - Timers List in Step Canvas
    - Prefab Digital Timer
    - Scroll Wheel Set Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TextMeshProUGUI TimerTxt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            TimeLeft -= Time.deltaTime;
+            updateTimer(TimeLeft);
+        }
+        else
+        {
+            Debug.Log("Time is UP!");
+            TimeLeft = 0;
+            TimerOn = false;
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
